54e00aca496b71b94d0bdc4544edb73cd0dad8eb
saddala yogesh <saddalayogesh@gmail.com>
2026-07-25 15:06:59 +0530

    feat: initial Spring Boot project scaffold with modular package structure

---
+++ truckbites-backend/src/test/java/com/truckbites/TruckBitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truckbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TruckBitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
